35d57a35e965c62e2b00aa951aad247691916c78
Dmitriy <skverdv@gmail.com>
2023-08-31 12:22:19 +0300

    User data

---
+++ FourthSim/Task22.py
@@ -0,0 +1,14 @@
+n=(int(input("Введите число N элементов: ")))
+num_list_1=[]
+for i in range(n):
+    num = int(input("Введите num "))
+    num_list_1.append(num)
+print(num_list_1)
+
+m=(int(input("Введите число M элементов: ")))
+num_list_2 = []
+for i in range(m):
+    num = int(input("Введите num "))
+    num_list_2.append(num)
+print(num_list_2)
+
