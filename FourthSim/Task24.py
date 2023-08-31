2662448933536f23ef335000318e17f3aa76c93e
Dmitriy <skverdv@gmail.com>
2023-08-31 13:07:44 +0300

    Decision24

--- FourthSim/Task24.py
+++ FourthSim/Task24.py
@@ -4,4 +4,7 @@ for i in range(n_bushes):
     a =int(input('Введите количество ягод на кусте: '))
     arr.append(a)
 
-
+arr_count = list()
+for i in range(len(arr)):
+       arr_count.append(arr[i-2] + arr[i-1] + arr[i])
+print(max(arr_count))
