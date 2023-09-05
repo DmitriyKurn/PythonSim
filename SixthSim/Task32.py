1f2a34051d44fcc44ef6ba3df402198b14e5f15d
Dmitriy <skverdv@gmail.com>
2023-09-05 19:09:38 +0300

    Data

---
+++ SixthSim/Task32.py
@@ -0,0 +1,10 @@
+n=(int(input("Введите число N элементов: ")))
+list_1=[]
+for i in range(n):
+    num = int(input("Введите num "))
+    list_1.append(num)
+print(list_1)
+
+max = (int(input("Введите max: ")))
+min = (int(input("Введите min: ")))
+
