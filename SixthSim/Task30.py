d9491be9dfb75695d0079fae9d4954ce539baeb3
Dmitriy <skverdv@gmail.com>
2023-09-05 18:29:35 +0300

    Decision30

--- SixthSim/Task30.py
+++ SixthSim/Task30.py
@@ -2,3 +2,5 @@ a1 = int(input('Введите первый элемент: '))
 n = int(input('Введите количество элементов: '))
 d = int(input('Введите разность: '))
 
+for i in range(n):
+     print(a1 + i * d, end=' ')
